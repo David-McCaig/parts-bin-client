4683f0df6e204d62a2d99691e14f3b253f57350c
David-McCaig <davidmccaig1@gmail.com>
2023-06-02 12:03:36 +1200

    Add image preview with Tailwind

--- src/Components/Upload/Upload.jsx
+++ src/Components/Upload/Upload.jsx
@@ -129,13 +129,21 @@ const Upload = () => {
       <form onSubmit={handleSubmit((data) => {
         return handleUpdateSaved(data)
       })} className="upload-add-form">
-        {/*Add image*/}
-
-        <img src={imagePreview} alt='' width="100%" className="image" ></img>
-        <input type="file" name="image_path" className="image__button" onChange={handleFileChange} />
 
         <div className="upload-add-form__item-details">
           <div className="upload-add-form__main-header"></div>
+      {/* Add Image */}
+        {!imagePreview ? <div className="flex w-full mt-28 mb-20 items-center justify-center bg-grey-lighter">
+          <label className="w-64 flex flex-col items-center px-4 py-6 bg-white hover:bg-sky-700  text-sky-500/100 rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer  hover:text-sky-500/75">
+            <svg className="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+            </svg>
+            <span class="mt-2 text-base leading-normal">Select a file</span>
+            <input type='file' className="hidden" name="image_path" onChange={handleFileChange} />
+          </label>
+        </div>
+          :
+          <img src={imagePreview} alt='' width="100%" className="flex w-full mt-14 mb-8 items-center justify-center bg-grey-lighter w-200 rounded" ></img>}
 
           {/* Input price */}
           <div>
